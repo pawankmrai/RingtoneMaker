bfa0230a765fd782460f4ee04ed7182190e37b37
Pawan <ghz.rai@gmail.com>
2013-02-18 16:06:58 +0530

    Initial Commit

---
+++ RingtoneMaker/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  RingtoneMaker
+//
+//  Created by Sandeep Nasa on 2/18/13.
+//  Copyright (c) 2013 Pawan Rai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RingtoneMaker/RMViewController.m
@@ -0,0 +1,29 @@
+//
+//  RMViewController.m
+//  RingtoneMaker
+//
+//  Created by Sandeep Nasa on 2/18/13.
+//  Copyright (c) 2013 Pawan Rai. All rights reserved.
+//
+
+#import "RMViewController.h"
+
+@interface RMViewController ()
+
+@end
+
+@implementation RMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
